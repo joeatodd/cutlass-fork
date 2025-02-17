e7d9c6e2b8453c93b23cbafdb14d82374a979248
Joe Todd <joe.todd@codeplay.com>
2025-02-17 12:21:28 +0000

    Finalize reproducer

--- examples/sycl/pvc/loop_hoist_repro.cpp
+++ examples/sycl/pvc/loop_hoist_repro.cpp
@@ -1,3 +1,13 @@
+// This reproducer demonstrates an issue with decomposed 2D block load operations.
+// The code calls two 1x16 block load operfations within a dynamic-sized loop.
+// The input data are such that each thread *should* receive two values equal to
+// the sg-local thread ID, and the same + 16
+//
+// If  IGC_allowDecompose2DBlockFuncs=0, this passes, but if
+// IGC_allowDecompose2DBlockFuncs=1, it fails:
+//
+// clang++ -fsycl -fsycl-targets=intel_gpu_pvc loop_hoist_repro.cpp && ./a.out
+
 #include <sycl/sycl.hpp>
 
 #ifdef __SYCL_DEVICE_ONLY__
@@ -11,28 +21,12 @@ using coord_t = vector_t<int, 2>;
 #ifdef __SYCL_DEVICE_ONLY__
 #define SYCL_DEVICE_BUILTIN(x) SYCL_EXTERNAL extern "C" x
 #else
-#define SYCL_DEVICE_BUILTIN(x)                                                 \
-  inline x {                                                                   \
-    return 0;                                                                  \
-  }
+#define SYCL_DEVICE_BUILTIN(x) inline x { return 0; }
 #endif
 
 SYCL_DEVICE_BUILTIN(ushort __builtin_IB_subgroup_block_read_flat_u16_m1k16v1(
     long baseoffset, int width_minus_one, int height_minus_one,
     int pitch_minus_one, coord_t coord));
-
-struct XE_2D_U16x1x16_LD_N {
-
-  template <class T>
-   static void copy(const void *baseoffset, int width,
-                                    int height, int pitch, coord_t coord,
-                                    T *dst) {
-    static_assert(sizeof(T) == 2, "Expected T to have size 2");
-    *dst = sycl::bit_cast<T>(
-        __builtin_IB_subgroup_block_read_flat_u16_m1k16v1(
-            (long)(baseoffset), width - 1, height - 1, pitch - 1, coord));
-  }
-};
 
 int main(int argc, const char** argv)
 {
@@ -62,13 +56,15 @@ int main(int argc, const char** argv)
 
     for (int j = 0; j < dynamic_loop_range; ++j){
       for (int i = 0; i < 2; ++i){
-        XE_2D_U16x1x16_LD_N::copy<T>(dev_T, width, height, pitch, coord_arr[i], &copy_result[i]);
+        copy_result[i] = sycl::bit_cast<T>(
+            __builtin_IB_subgroup_block_read_flat_u16_m1k16v1(
+                (long)(dev_T), width - 1, height - 1, pitch - 1, coord_arr[i]));
       }
     }
 
     int thread_id = item.get_global_linear_id();
-    int sg_id = thread_id % 16;
-    dev_success[thread_id] = (copy_result[0] == sg_id && copy_result[1] == sg_id + 16);
+    int sg_local_id = thread_id % 16;
+    dev_success[thread_id] = copy_result[0] == sg_local_id && copy_result[1] == sg_local_id + 16;
   }).wait();
 
   std::vector<int> host_success(thread_count);
