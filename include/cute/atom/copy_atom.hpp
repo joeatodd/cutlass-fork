77deffb9631e974880f68ee58606f2be7e0c1260
Joe Todd <joe.todd@codeplay.com>
2025-03-10 11:26:05 +0000

    Remove debug prints

--- include/cute/atom/copy_atom.hpp
+++ include/cute/atom/copy_atom.hpp
@@ -1,4 +1,3 @@
-#define PRINT(x) print(#x ": "); print(x); print("\n");
 /***************************************************************************************************
  * Copyright (c) 2023 - 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
@@ -261,20 +260,6 @@ struct TiledCopy : Copy_Atom
     // Transform the tile mode
     auto v_tensor = t_tensor.compose(frg_layout_v, _);
     // ((atom_vals,rest_vals),(1,RM,RN,...))
-  // TODO(joe): Tidy this up
-    if (false && cute::thread0()) {
-      cute::print("\n");
-      PRINT(tensor);
-      PRINT(TiledNumThr{});
-      PRINT(TiledLayout_TV{});
-      PRINT(V);
-      PRINT(frg_layout_mn);
-      PRINT(frg_layout_v);
-      PRINT(t_tensor);
-      PRINT(v_tensor);
-      PRINT(v_tensor(_, append<R>(Int<0>{},_)));
-      cute::print("\n");
-    }
 
     // Unfold and return
     return v_tensor(_, append<R>(Int<0>{},_));
